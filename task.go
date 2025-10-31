61d5f92bda9741b817aafe5d067156ce63cf836e
anushkaRazor <anushka.gupta@razorpay.com>
2025-10-31 10:50:22 +0530

    Add files via upload

--- task.go
+++ task.go
@@ -1,9 +1,14 @@
 package main
 
+import "sync"
+
 type Task struct {
 	Description string `json:"description"`
 	OwnerId     int    `json:"owner_id"`
 	IsCompleted bool   `json:"is_completed"`
 }
 
-var tasks []Task
+var (
+	tasks []Task
+	mutex sync.RWMutex
+)
